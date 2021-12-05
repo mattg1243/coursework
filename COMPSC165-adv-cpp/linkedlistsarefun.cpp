1b51401630e430d949ab915056dfda2458d5ca2e
mattg1243 <mattgallucci97@gmail.com>
2021-12-04 16:20:36 -0800

    push_back, front and back methods functioning properly

--- COMPSC165-adv-cpp/linkedlistsarefun.cpp
+++ COMPSC165-adv-cpp/linkedlistsarefun.cpp
@@ -21,9 +21,73 @@ class MyDeque {
     void remove(int);
     void clear();
     friend ostream& operator<<(ostream&, const MyDeque&);
+
 };
 
+int MyDeque::front() const { 
+    
+    if (head == nullptr) { return -1; }
+    return head->x;
+}
+
+int MyDeque::back() const { 
+    
+    if (head == nullptr) { return -1; }
+
+    Node* search = head;
+    while (search->next != nullptr) { 
+        search = search->next;
+    }
+
+    return search->x;
+}
+
+void MyDeque::push_back(int v) {
+
+    Node* newPtr, *search = head;
+    newPtr = new Node;
+    newPtr->x = v;
+    newPtr->next = nullptr;
+
+    if (head != nullptr) {
+        while (search->next != nullptr) { 
+            search = search->next;
+        }
+        search->next = newPtr;
+    } else {
+        head = newPtr;
+    }
+}
+
+void MyDeque::push_front(int v) {
+
+}
+
+int MyDeque::size() const {
+    return 0;
+}
+
+void MyDeque::remove(int v) {
+
+}
+
+void MyDeque::clear() {
+
+}
+
+ostream& operator<<(ostream& o, const MyDeque& m) {
+    return o;
+}
+
 int main() {
+
+    MyDeque dq;
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);
+
+    cout << "Front: " << dq.front() << endl;
+    cout << "Back: " << dq.back() << endl;
 
     return 0;
 }
